0500d360c8e2188b38ac656803daf0520cdf7891
sheila <sheilatimp@gmail.com>
2024-08-07 17:44:14 +0200

    app push

--- app.py
+++ app.py
@@ -3,18 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# Define UI
 app_ui = ui.page_fluid(
     ui.layout_sidebar(
-        ui.panel_sidebar(
+        ui.sidebar(
             ui.h2("Page Charts"),
             ui.hr(),
             ui.input_slider(id="slider", label="Number of bins:", min=5, max=25, value=15)
         ),
-        ui.panel_main(
+        ui.page_fluid(
             ui.output_plot(id="histogram")
         )
     )
 )
+
 
 def server(input, output, session):
     @output
@@ -23,7 +25,6 @@ def server(input, output, session):
         x = 100 + np.random.randn(500)
         plt.title("A histogram", size=20)
         plt.hist(x=x, bins=input.slider(), color="grey", ec="black")
-
 
 
 app = App(ui=app_ui, server=server)
